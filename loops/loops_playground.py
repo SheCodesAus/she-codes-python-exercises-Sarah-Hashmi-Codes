f1d5edc23d0b5527594f17ea0ab0942770d47c16
Sarah-Hashmi-Codes <114784014+Sarah-Hashmi-Codes@users.noreply.github.com>
2022-11-02 16:35:23 +1100

    Update loops_playground.py

---
+++ loops/loops_playground.py
@@ -0,0 +1,102 @@
+# for loop
+# my_iterable = [1, 2, 3,4,5,6,7,8]
+
+# for item in my_iterable:
+#     #some code here
+#     print(item)
+
+# for num in range(1, 10):  (start:Stop)
+    # print(num)
+
+
+# for num in range(1,11,2):  (start:Stop:step)
+#     print(num)
+
+# for num in range(0,101):
+#     print(num)
+
+# for num in range(0,101,5):
+#     print(num)
+
+# my_name = "Sarah"
+
+# for letter in my_name:
+#     print(letter)
+
+wishlist = ["igloo", "chicken", "donut toy"]
+
+# for item in range(len(wishlist)):
+#     print(wishlist[item])
+
+# for item in wishlist:
+#     print(f"Chilli wants: {item}")
+
+# chilli_wishlist = [["igloo"],["donut toy", "tennis ball", "crocodile toy"],["chicken", "peanut butter"],["cardboard box", "kong", "dig mat"]]
+
+
+# for category in chilli_wishlist:
+#     for item in category:print(item)
+
+#While Loops
+
+# some_boolean_condition = True
+# while some_boolean_condition:
+#     #do somethin
+
+# counter = 10
+# while counter <= 10:   
+#     print(counter)
+#     #counter = counter + 1 
+#     counter += 1
+
+
+# guess = ""
+# while guess != "a":
+#     guess = input("Guess a letter:")
+
+
+# num = int(input("Enter a number: "))
+
+# for i in range(1,num+1):
+#     print(f"{num} * {i} = {num*i}")
+
+
+# num = int(input("Enter a number : "))
+# total = 0
+# for i in range(1,num+1):
+#     total += i
+
+# print(total)
+
+
+# N = int(input("Please input an integer: "))
+# i = 1
+# x = range(1, N + 1)
+# for i in x:
+#     nums = range(1, i + 1)
+# print(int(sum(nums)))
+
+
+# num = [3, 5, 9, 1] 
+# for i in range(0,1):
+#     print(sum(num))
+
+# num = [-3, -5, 9, 1]
+# for i in range(0,1):
+#     print(sum(num))
+
+mailing_list = [
+["Chilli", "chilli@thechihuahua.com"],
+["Roary", "roary@moth.catchers"],
+["Remus", "remus@kapers.dog"],
+["Prince Thomas of Whitepaw", "hrh.thomas@royalty.wp"],
+["Ivy", "noreply@goldendreamers.xyz"],
+]
+
+for i in mailing_list(0,1):
+    print(mailing_list)
+    
+    
+
+
+
